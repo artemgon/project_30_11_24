4bb6d53e5d442aec82bef3ebfbf7e348419592d4
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 11:53:10 +0200

    Master branch has been prepared.

---
+++ Classes/Classes.cs
@@ -0,0 +1,7 @@
+﻿
+namespace project_30_11_24.Classes
+{
+    internal class Classes
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace project_30_11_24
+﻿using project_30_11_24.Classes;
+
+namespace project_30_11_24
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
