34c3c4dc8f26034d006a82084e202a25c28808ae
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 13:10:01 +0200

    Task 1 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,50 @@
 ﻿
 namespace project_30_11_24.Classes
 {
-    internal class Classes
+    public class Money
     {
+        public string? Currency { get; set; }
+        public int IntAmount { get; set; }
+        public int Coins { get; set; }
+        public Money()
+        {
+            Currency = "USD";
+            IntAmount = 0;
+            Coins = 0;
+        }
+        public Money(string currency, int intAmount, int coins)
+        {
+            Currency = currency;
+            IntAmount = intAmount;
+            Coins = coins;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Currency: {Currency}, Amount: {IntAmount}, Coins: {Coins}");
+        }
+    }
+    public class Product : Money
+    {
+        public string? Name { get; set; }
+        public Product()
+        {
+            Name = "Unknown";
+        }
+        public Product(string name, string currency, int intAmount, int coins) : base(currency, intAmount, coins)
+        {
+            Name = name;
+        }
+        public void Decrease(int sum)
+        {
+            if (sum > IntAmount)
+            {
+                throw new Exception("Not enough money");
+            }
+            IntAmount -= sum;
+        }
+        public new void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Currency: {Currency}, Amount: {IntAmount}, Coins: {Coins}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,24 @@ namespace project_30_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Money money = new Money
+                {
+                    Currency = "USD",
+                    IntAmount = 100,
+                    Coins = 50
+                };
+                money.Print();
+                Product product = new Product
+                {
+                    Name = "Product",
+                    Currency = "USD",
+                    IntAmount = 50,
+                    Coins = 25
+                };
+                product.Print();
+                product.Decrease(10);
+                product.Print();
+                Console.WriteLine();
             }
             catch (Exception e)
             {
