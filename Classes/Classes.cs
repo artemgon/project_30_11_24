8dcbd89748e5e4be7dbbc3b7fa30a958f3dac6f0
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 12:26:55 +0200

    Task 2 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,66 @@
 ﻿
 namespace project_30_11_24.Classes
 {
-    internal class Classes
+    public class Passport
     {
+        public string? SNP { get; set; }
+        public string? Sex { get; set; }
+        public string? BirthDate { get; set; }
+        public int Age { get; set; }
+        public string? Number { get; set; }
+        public Passport()
+        {
+            SNP = "";
+            Sex = "";
+            BirthDate = "";
+            Age = 0;
+            Number = "";
+        }
+        public Passport(string sNP, string sex, string birthDate, int age, string number)
+        {
+            SNP = sNP;
+            Sex = sex;
+            BirthDate = birthDate;
+            Age = age;
+            Number = number;
+        }
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Passport Info:");
+            Console.WriteLine($"SNP: {SNP}");
+            Console.WriteLine($"Sex: {Sex}");
+            Console.WriteLine($"BirthDate: {BirthDate}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Number: {Number}");
+            Console.WriteLine();
+        }
+    }
+    public class ForeignPassport : Passport
+    {
+        public string? VisaData { get; set; }
+        public string? FNumber { get; set; }
+        public ForeignPassport()
+        {
+            VisaData = "";
+            FNumber = "";
+        }
+        public ForeignPassport(string VisaData, string FNumber)
+        {
+            this.VisaData = VisaData;
+            this.FNumber = FNumber;
+        }
+        public new void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Foreign Passport Info:");
+            Console.WriteLine($"SNP: {SNP}");
+            Console.WriteLine($"Sex: {Sex}");
+            Console.WriteLine($"BirthDate: {BirthDate}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Visa Data: {VisaData}");
+            Console.WriteLine($"Foreign Number: {FNumber}");
+            Console.WriteLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,26 @@ namespace project_30_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Passport passport = new Passport
+                {
+                    SNP = "Ivanov Ivan Ivanovich",
+                    Sex = "Male",
+                    BirthDate = "01.01.2000",
+                    Age = 25,
+                    Number = "123456789",
+                };
+                passport.Print();
+                ForeignPassport foreignPassport = new ForeignPassport
+                {
+                    SNP = "Ivanov Ivan Ivanovich",
+                    Sex = "Male",
+                    BirthDate = "01.01.2000",
+                    Age = 25,
+                    Number = "123456789",
+                    VisaData = "01.01.2022",
+                    FNumber = "987654321",
+                };
+                foreignPassport.Print();
             }
             catch (Exception e)
             {
