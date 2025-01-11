5a8be2e469b3418c2806eb5b01bf139751c0afb2
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 12:37:39 +0200

    Task 3 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,86 @@
 ﻿
 namespace project_30_11_24.Classes
 {
-    internal class Classes
+    public class Animal
     {
+        public string? Name { get; set; }
+        public string? Region { get; set; }
+        public int Speed { get; set; }
+        public int Age { get; set; }
+        public Animal()
+        {
+            Name = "Unknown";
+            Region = "Unknown";
+            Speed = 0;
+            Age = 0;
+        }
+        public Animal(string name, string region, int speed, int age)
+        {
+            Name = name;
+            Region = region;
+            Speed = speed;
+            Age = age;
+        }
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Animal Info: ");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Region: {Region}");
+            Console.WriteLine($"Speed: {Speed}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine();
+        }
+    }
+    public class Tiger : Animal
+    {
+        public Tiger() : base()
+        {
+            Name = "Tiger";
+            Region = "Asia";
+            Speed = 60;
+            Age = 10;
+        }
+        public Tiger(string name, string region, int speed, int age) : base(name, region, speed, age)
+        {
+            Name = name;
+            Region = region;
+            Speed = speed;
+            Age = age;
+        }
+    }
+    public class Crocodile : Animal
+    {
+        public Crocodile() : base()
+        {
+            Name = "Crocodile";
+            Region = "Africa";
+            Speed = 30;
+            Age = 20;
+        }
+        public Crocodile(string name, string region, int speed, int age) : base(name, region, speed, age)
+        {
+            Name = name;
+            Region = region;
+            Speed = speed;
+            Age = age;
+        }
+    }
+    public class Kangaroo : Animal
+    {
+        public Kangaroo() : base()
+        {
+            Name = "Kangaroo";
+            Region = "Australia";
+            Speed = 70;
+            Age = 15;
+        }
+        public Kangaroo(string name, string region, int speed, int age) : base(name, region, speed, age)
+        {
+            Name = name;
+            Region = region;
+            Speed = speed;
+            Age = age;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace project_30_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Animal animal = new Animal("Animal", "Unknown", 0, 0);
+                animal.Print();
+                Tiger tiger = new Tiger("Tiger", "Asia", 60, 10);
+                tiger.Print();
+                Crocodile crocodile = new Crocodile("Crocodile", "Africa", 30, 20);
+                crocodile.Print();
+                Kangaroo kangaroo = new Kangaroo("Kangaroo", "Australia", 70, 15);
+                kangaroo.Print();
             }
             catch (Exception e)
             {
