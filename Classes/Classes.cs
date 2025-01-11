c265a88f16f70f0b14a29103e40477460a7ea7ea
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 12:07:02 +0200

    Task 1 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,161 @@
 ﻿
 namespace project_30_11_24.Classes
 {
-    internal class Classes
+    public class Human
     {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Salary { get; set; }
+        public int Age { get; set; }
+        public Human()
+        {
+            Name = "";
+            Surname = "";
+            Salary = "";
+            Age = 0;
+        }
+        public Human(string name) : this()
+        {
+            Name = name;
+        }
+        public Human(string name, string surname) : this(name)
+        {
+            Surname = surname;
+        }
+        public Human(string name, string surname, string salary) : this(name, surname)
+        {
+            Salary = salary;
+        }
+        public Human(string name, string surname, string salary, int age) : this(name, surname, salary)
+        {
+            Age = age;
+        }
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Human info:");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Salary: {Salary}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine();
+        }
+    }
+    public class Builder : Human
+    {
+        public string? Company { get; set; }
+        public Builder() : base()
+        {
+            Company = "";
+        }
+        public Builder(string name) : base(name)
+        {
+            Company = "";
+        }
+        public Builder(string name, string surname) : base(name, surname)
+        {
+            Company = "";
+        }
+        public Builder(string name, string surname, string salary) : base(name, surname, salary)
+        {
+            Company = "";
+        }
+        public Builder(string name, string surname, string salary, int age) : base(name, surname, salary, age)
+        {
+            Company = "";
+        }
+        public Builder(string name, string surname, string salary, int age, string company) : base(name, surname, salary, age)
+        {
+            Company = company;
+        }
+        public new void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Builder info:");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Salary: {Salary}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Company: {Company}");
+            Console.WriteLine();
+        }
+    }
+    public class Sailor : Human
+    {
+        public string? Ship { get; set; }
+        public Sailor() : base()
+        {
+            Ship = "";
+        }
+        public Sailor(string name) : base(name)
+        {
+            Ship = "";
+        }
+        public Sailor(string name, string surname) : base(name, surname)
+        {
+            Ship = "";
+        }
+        public Sailor(string name, string surname, string salary) : base(name, surname, salary)
+        {
+            Ship = "";
+        }
+        public Sailor(string name, string surname, string salary, int age) : base(name, surname, salary, age)
+        {
+            Ship = "";
+        }
+        public Sailor(string name, string surname, string salary, int age, string ship) : base(name, surname, salary, age)
+        {
+            Ship = ship;
+        }
+        public new void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sailor info:");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Salary: {Salary}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Ship: {Ship}");
+            Console.WriteLine();
+        }
+    }
+    public class Pilot : Human
+    {
+        public string? Plane { get; set; }
+        public Pilot() : base()
+        {
+            Plane = "";
+        }
+        public Pilot(string name) : base(name)
+        {
+            Plane = "";
+        }
+        public Pilot(string name, string surname) : base(name, surname)
+        {
+            Plane = "";
+        }
+        public Pilot(string name, string surname, string salary) : base(name, surname, salary)
+        {
+            Plane = "";
+        }
+        public Pilot(string name, string surname, string salary, int age) : base(name, surname, salary, age)
+        {
+            Plane = "";
+        }
+        public Pilot(string name, string surname, string salary, int age, string plane) : base(name, surname, salary, age)
+        {
+            Plane = plane;
+        }
+        public new void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pilot info:");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Salary: {Salary}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Plane: {Plane}");
+            Console.WriteLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,41 @@ namespace project_30_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Human human = new Human
+                {
+                    Name = "John",
+                    Surname = "Doe",
+                    Salary = "1000",
+                    Age = 30
+                };
+                human.Print();
+                Builder builder = new Builder
+                {
+                    Name = "Jane",
+                    Surname = "Doe",
+                    Salary = "2000",
+                    Age = 25,
+                    Company = "Company"
+                };
+                builder.Print();
+                Sailor sailor = new Sailor
+                {
+                    Name = "Jack",
+                    Surname = "Sparrow",
+                    Salary = "3000",
+                    Age = 35,
+                    Ship = "Black Pearl"
+                };
+                sailor.Print();
+                Pilot pilot = new Pilot
+                {
+                    Name = "John",
+                    Surname = "Travolta",
+                    Salary = "4000",
+                    Age = 40,
+                    Plane = "Boeing 747"
+                };
+                pilot.Print();
             }
             catch (Exception e)
             {
